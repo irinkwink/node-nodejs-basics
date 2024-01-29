4207921ac4e40f4f53303dd628c38084662e76ec
irinkwink <irina-ns@yandex.ru>
2024-01-29 21:20:40 +0300

    feat: implement streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import * as fs from 'fs';
+import { fileURLToPath } from 'url'
+import { resolve, dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = resolve(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream(filePath, { encoding: 'utf-8' });
+
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
+
+    readStream.on('end', () => {
+        process.stdout.write('\n');
+    });
 };
 
 await read();
