6dc27c5445724adf6c7bd9e7f1b84fd33df6c1bb
irinkwink <irina-ns@yandex.ru>
2024-01-29 19:11:27 +0300

    feat: implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import * as fs from 'fs';
+import { fileURLToPath } from 'url'
+import { resolve, dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const originalFolderPath = resolve(__dirname, 'files');
+const copyFolderPath = resolve(__dirname, 'files_copy');
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        fs.accessSync(originalFolderPath);
+        try {
+            fs.accessSync(copyFolderPath);
+        } catch (err) {
+            fs.cpSync(originalFolderPath, copyFolderPath, { recursive: true })
+            return;
+        }
+        throw new Error('FS operation failed');
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await copy();
