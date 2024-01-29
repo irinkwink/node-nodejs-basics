d92afd83057d3841328bf5a49a40e53aceafc00b
irinkwink <irina-ns@yandex.ru>
2024-01-29 23:42:27 +0300

    feat: implement cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url'
+import { resolve, dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const scriptPath = resolve(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = spawn('node', [scriptPath, ...args], { stdio: ['pipe', 'pipe', 'inherit'] });
+
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
+
+    childProcess.on('exit', (code) => {
+        console.log(`Child process exited with code ${code}`);
+        process.exit();
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 2, 'someArgument3', 'Argument4']);
