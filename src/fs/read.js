2f54190b1e0cedd411c3669ca2b38857c6d1d50c
irinkwink <irina-ns@yandex.ru>
2024-01-29 19:48:27 +0300

    feat: implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import * as fs from 'fs';
+import { fileURLToPath } from 'url'
+import { resolve, dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = resolve(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    try {
+        fs.accessSync(filePath);
+        const content = fs.readFileSync(filePath, {encoding: 'utf-8'});
+        console.log(content);
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
